ff22ac67fd49fd9d6b82c675d2ad652da4bc3fdc
roshy1234 <jamesroshy@gmail.com>
2024-07-08 10:48:35 +0530

    create fcfsdisk.c

---
+++ fcfsdisk.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main() {
+    int n,RQ[10],initial,thm=0,lr,sr,dir,size;
+    printf("Enter the number of requests -- ");
+    scanf("%d", &n);
+    printf("Enter the request sequence : \n");
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &RQ[i]);
+    }
+    printf("Enter the initial head position -- ");
+    scanf("%d", &initial);
+    //fcfs disk scheduling algorithm
+
+    for(int i=0;i<n;i++) {
+        thm += abs(initial - RQ[i]);
+        initial = RQ[i];
+}
+}
