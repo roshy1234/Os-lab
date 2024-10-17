3555591fdb04753aa714e3eab43b574a712c2884
roshy1234 <117187383+roshy1234@users.noreply.github.com>
2024-10-17 13:27:04 +0530

    Update rr2.c with arrival time

--- rr2.c
+++ rr2.c
@@ -5,52 +5,55 @@ struct process {
 } P[20];
 
 void main() {
-    int i = 0, time = 0, n, tq;
-    printf("Enter the number of processes: ");
+    int i = 0, x = 0, y = 0, n, tq;
+    printf("Enter the number of processes -- ");
     scanf("%d", &n);
-    printf("Enter the time quantum: ");
+    printf("Enter the time quantum -- ");
     scanf("%d", &tq);
-    for (i = 0; i < n; i++) {
-        printf("Enter arrival time of process %d: ", i + 1);
-        scanf("%d", &P[i].at);
-        printf("Enter burst time of process %d: ", i + 1);
-        scanf("%d", &P[i].bt);
+    for(i = 0; i < n; i++) {
+        printf("Enter the arrival time and burst time of process %d -- ", i + 1);
+        scanf("%d %d", &P[i].at, &P[i].bt);
         P[i].id = i + 1;
         P[i].tbt = P[i].bt;
-        P[i].wt = 0;
-        P[i].tat = 0;
     }
-
-    int remaining = n;
-    printf("Gantt Chart:\n");
-    while (remaining > 0) {
-        int done = 1;
-        for (i = 0; i < n; i++) {
-            if (P[i].at <= time && P[i].bt > 0) {
-                done = 0;
-                if (P[i].bt > tq) {
-                    time += tq;
-                    P[i].bt -= tq;
-                    printf("P%d ", P[i].id);
-                } else {
-                    time += P[i].bt;
-                    P[i].bt = 0;
-                    P[i].tat = time - P[i].at; // Turnaround time = completion time - arrival time
-                    P[i].wt = P[i].tat - P[i].tbt; // Waiting time = Turnaround time - burst time
-                    printf("P%d ", P[i].id);
-                    remaining--;
+    printf("Gantt Chart -- \n");
+    while(1) {
+        int j, min_at = -1;
+        for(j = 0; j < n; j++) {
+            if(P[j].bt > 0) {
+                if(min_at == -1 || P[j].at < min_at) {
+                    min_at = P[j].at;
                 }
             }
         }
-        if (done == 1) {
-            time++;
+        if(min_at != -1 && min_at > x) {
+            x = min_at;
         }
-    }
+        if(P[i].bt > 0 && P[i].at <= x) {
+            if(P[i].bt > tq) {
+                P[i].bt -= tq;
+                x += tq;
+                printf("P%d ", P[i].id);
+            }
+            else {
+                x += P[i].bt;
+                P[i].bt = 0;
 
-    printf("\nProcess\tAT\tBT\tTAT\tWT\n");
-    for (i = 0; i < n; i++) {
+                P[i].tat = x - P[i].at; // TAT = completion time - arrival time
+                P[i].wt = P[i].tat - P[i].tbt; // WT = TAT - burst time
+                printf("P%d ", P[i].id);
+                y++;
+                if(y == n) break;
+            }
+        }
+        i = (i + 1) % n; // move to next process even if current process has 0 burst time
+    }
+    
+    printf("\nProcess\tBT\tAT\tTAT\tWT\n");
+    for(i = 0; i < n; i++) {
         printf("%d\t%d\t%d\t%d\t%d\n", P[i].id, P[i].at, P[i].tbt, P[i].tat, P[i].wt);
     }
+
 }
 
 
