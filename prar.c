32a16f4ef8ec929fdda416ce1984609ef8288c04
roshy1234 <117187383+roshy1234@users.noreply.github.com>
2024-10-17 12:22:11 +0530

    Update prar.c with arrival time

--- prar.c
+++ prar.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 
 struct process {
@@ -8,39 +9,39 @@ void main() {
     int n;
     printf("Enter the number of processes -- ");
     scanf("%d", &n);
-    for(int i = 0; i < n; i++) {
-        printf("Enter the priority, arrival time and burst time of process %d -- ", i + 1);
-        scanf("%d %d %d", &P[i].pr, &P[i].at, &P[i].bt);
+    for (int i = 0; i < n; i++) {
+        printf("Enter the arrival time, priority, and burst time of process %d -- ", i + 1);
+        scanf("%d %d %d", &P[i].at, &P[i].pr, &P[i].bt);
         P[i].id = i + 1;
     }
-    for(int i=0;i<n-1;i++){
-        for(int j=0;j<n-i-1;j++){
-            if(P[j].pr > P[j+1].pr){
+
+    // Sorting processes based on priority
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (P[j].pr > P[j + 1].pr) {
                 temp = P[j];
-                P[j] = P[j+1];
-                P[j+1] = temp;
+                P[j] = P[j + 1];
+                P[j + 1] = temp;
             }
         }
     }
 
-P[0].wt = 0;
-P[0].tat = P[0].bt - P[0].at;
-for(int i = 1; i < n; i++) {
-    P[i].wt = P[i-1].tat + P[i-1].at - P[i].at;
-    P[i].tat = P[i].wt + P[i].bt;
-}
+    int time = 0;
+    printf("Gantt Chart -- ");
+    for (int i = 0; i < n; i++) {
+        if (time < P[i].at) {
+            time = P[i].at; // Adjust time to the arrival time of the next process
+        }
+        printf("P%d ", P[i].id);
+        P[i].wt = time - P[i].at;
+        time += P[i].bt;
+        P[i].tat = time - P[i].at;
+    }
 
-printf("Gantt Chart -- ");
-int x=0;
-for(int i = 0; i < n; i++) {
-    while(x < P[i].at) x++;
-    printf("P%d ", P[i].id);
-    x += P[i].bt;
-}
-
-    //Displaying the table
-    printf("\nProcess\tpr\tAT\tBT\tTAT\tWT\n");
-    for(int i = 0; i < n; i++) {
-        printf("%d\t%d\t%d\t%d\t%d\t%d\n", P[i].id,P[i].pr,P[i].at,P[i].bt, P[i].tat, P[i].wt);
+    // Displaying the table
+    printf("\nProcess\tAT\tPR\tBT\tTAT\tWT\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d\t%d\t%d\t%d\t%d\t%d\n", P[i].id, P[i].at, P[i].pr, P[i].bt, P[i].tat, P[i].wt);
     }
 }
+
