721991d50f0ed33c58012e18432974975e26b2b6
roshy1234 <117187383+roshy1234@users.noreply.github.com>
2024-10-17 09:32:03 +0530

    rr2.c with arrival time

--- rr2.c
+++ rr2.c
@@ -5,7 +5,7 @@ struct process {
 } P[20];
 
 void main() {
-    int i = 0, x = 0, y = 0, n, tq;
+    int i = 0, time = 0, n, tq;
     printf("Enter the number of processes: ");
     scanf("%d", &n);
     printf("Enter the time quantum: ");
@@ -17,11 +17,13 @@ void main() {
         scanf("%d", &P[i].bt);
         P[i].id = i + 1;
         P[i].tbt = P[i].bt;
+        P[i].wt = 0;
+        P[i].tat = 0;
     }
 
-    int time = 0;
+    int remaining = n;
     printf("Gantt Chart:\n");
-    while (1) {
+    while (remaining > 0) {
         int done = 1;
         for (i = 0; i < n; i++) {
             if (P[i].at <= time && P[i].bt > 0) {
@@ -36,11 +38,13 @@ void main() {
                     P[i].tat = time - P[i].at; // Turnaround time = completion time - arrival time
                     P[i].wt = P[i].tat - P[i].tbt; // Waiting time = Turnaround time - burst time
                     printf("P%d ", P[i].id);
-                    y++;
+                    remaining--;
                 }
             }
         }
-        if (done == 1) break;
+        if (done == 1) {
+            time++;
+        }
     }
 
     printf("\nProcess\tAT\tBT\tTAT\tWT\n");
@@ -48,3 +52,5 @@ void main() {
         printf("%d\t%d\t%d\t%d\t%d\n", P[i].id, P[i].at, P[i].tbt, P[i].tat, P[i].wt);
     }
 }
+
+
